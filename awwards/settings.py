060e461ce414685a47257974fd281b8f9ce342ef
Dennis Mwaniki <damos5776@gmail.com>
2021-09-19 20:02:11 +0300

    settings

--- awwards/settings.py
+++ awwards/settings.py
@@ -113,14 +113,7 @@ WSGI_APPLICATION = 'awwards.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'awards',
-        'USER': 'mwaniki',
-    'PASSWORD':'mwaniki',
-    }
-}
+
 
 
 # Password validation
